78de9f50d8a1a92ad456183105d037c01ef7e6c6
Ankush Kumar <kumarankush1709@gmail.com>
2022-09-03 11:03:03 +0530

    login signup done

--- src/firebase.js
+++ src/firebase.js
@@ -6,7 +6,8 @@ const app=initializeApp({
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_ID,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId:process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 })
 export const auth=getAuth(app);
 export default app
