958aaa7486e5c7a64198e8f1e79618b472efed62
Nanditha C P <84667237+Nanditha-C-P@users.noreply.github.com>
2022-09-03 17:22:48 +0530

    Update Login.js

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -66,7 +66,7 @@ const Login = () => {
             }}
           >
             <TextField
-              name="email"
+              name="Acharya Email"
               onChange={onChange}
               value={credentials.email}
               size="small"
